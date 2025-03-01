c17402db9849698a2623ad789587d74b69350050
lorenzo157 <lpzlorenzo12@gmail.com>
2025-03-01 00:52:50 -0300

    add new files: firts part

---
+++ back/src/user/dto/create-neighborhood.dto.ts
@@ -0,0 +1,11 @@
+export class Coordinates{
+    lat: number;
+    lng: number;
+}
+
+export class CreateNeighborhoodDto {  
+    idCity: number;
+    neighborhoodName: string;
+    numBlocksInNeighborhood: number;
+    coordinates: Coordinates[];
+  }
